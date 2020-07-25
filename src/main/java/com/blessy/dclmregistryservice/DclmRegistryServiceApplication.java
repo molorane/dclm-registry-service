a09b3a792fa1cc040ae14d99ffdf1fa1a873df87
Mothusi Molorane <molorane.mothusi@gmail.com>
2020-07-25 19:29:57 +0200

    first commit

---
+++ src/main/java/com/blessy/dclmregistryservice/DclmRegistryServiceApplication.java
@@ -0,0 +1,13 @@
+package com.blessy.dclmregistryservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DclmRegistryServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DclmRegistryServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/blessy/dclmregistryservice/DclmRegistryServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.blessy.dclmregistryservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DclmRegistryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
